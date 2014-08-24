a07edc05747815d3245a6c79f8167b35395d781b
Luiz Amorim <luiz.simples@gmail.com>
2014-08-24 02:33:31 -0300

    Code Coverage

--- api/coverage.sh
+++ api/coverage.sh
@@ -3,6 +3,8 @@
 node_modules/.bin/jscover api api-cov
 mv api api-orig
 mv api-cov api
-node_modules/.bin/mocha -R node_modules/mocha-lcov-reporter > coverage.lcov
+
+node_modules/.bin/mocha -R node_modules/mocha-lcov-reporter | node_modules/coveralls/bin/coveralls.js
+
 rm -rf api
 mv api-orig api
