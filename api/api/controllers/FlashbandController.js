fd7f2e069471e3a4992a4b0c3ff640f7fdb42110
Pablo Bender <pablo.bender@oncast.com.br>
2014-09-03 19:49:56 -0300

    inicio da implementação do FlashbandController

---
+++ api/api/controllers/FlashbandController.js
@@ -0,0 +1,14 @@
+// //var getFlashbandTag = function(req) {
+// //  return req.param('tag');
+// //};
+// 
+// module.exports = {
+//   block: function() { //(req, res) {
+//     console.log('not yet implemented');
+//     //FlashbandService.block(req.param('tag'))
+//     //.then(function(flashband) {
+//       //res.
+//     //});
+//     res.ok();
+//   }
+// };
--- api/test/integration/controllers/FlashbandController.test.js
+++ api/test/integration/controllers/FlashbandController.test.js
@@ -2,10 +2,40 @@
 //var shared       = require('../shared-specs');
 //var passwordHash = require('password-hash');
 //var FlashbandHelper = require('../../helpers/FlashbandHelper');
-
-//var args;
-//var serialToken;
-
+//
 //describe('FlashbandController', function() {
-  //describe('block')
+//
+//  var serialToken;
+//
+//  describe('#block', function() {
+//    beforeEach(function(done) {
+//      User.create({password: '123123123'}, function(err, user) {
+//        serialToken = passwordHash.generate(user.id);
+//        user.tokens.add({ token: serialToken });
+//        user.save(done);
+//      }, done);
+//    });
+//
+//    shared.shoudRequestNotFound('/flashband/block', ['GET', 'PUT', 'DELETE']);
+//
+//    it ('should block an existing flashband', function(done) {
+//      FlashbandHelper.createSuccess().then(function(validFlashband) {
+//        request(sails.hooks.http.app)
+//        .post('/flashband/block')
+//        .send({tag: validFlashband.tag})
+//        .expect(200, { message: 'Flashband blocked.' })
+//        .set('Authorization', 'Token token='.concat(serialToken))
+//        .end(done);
+//      }, done);
+//    });
+//    
+//    it('should not found a non existing flashband', function(done) {
+//      request(sails.hooks.http.app)
+//        .post('/flashband/block')
+//        .send({tag: 'non-existing'})
+//        .expect(403, 'Flashband not found.')
+//        .set('Authorization', 'Token token='.concat(serialToken))
+//        .end(done);
+//    });
+//  });
 //});
