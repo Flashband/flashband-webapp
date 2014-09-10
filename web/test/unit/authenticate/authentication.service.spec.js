c34921b9bd30928d916653cbdd064c599109d561
Luiz Amorim <luiz.simples@gmail.com>
2014-09-10 15:07:23 -0300

    Serviço Login tested

--- web/test/unit/authenticate/authentication.service.spec.js
+++ web/test/unit/authenticate/authentication.service.spec.js
@@ -3,24 +3,50 @@
 describe('Service: Login', function() {
   beforeEach(module('flashbandWebapp'));
 
-  it('should get a flashband', inject(function(_$httpBackend_, AuthenticateSrvc) {
-      var credencials = {
-        email: 'email@valid.net.lcc',
-        password: 'passwordSample'
-      };
+  var spyFail,
+      spySuccessfully,
+      authPromise,
+      credencials,
+      $httpBackend;
 
-      _$httpBackend_.whenPOST('http://localhost:1337/authenticate', credencials).respond(202, {
-        token: 'the user authentication token',
-        user: 10,
-        createdAt: '09/04/2014 01:15:27 AM (-0300)',
-        updatedAt: '09/04/2014 01:15:27 AM (-0300)'
-      });
+  beforeEach(inject(function(_$httpBackend_, AuthenticateSrvc) {
+    $httpBackend = _$httpBackend_;
 
-      var promise = AuthenticateSrvc.login(credencials);
-      var result = promise.$object;
+    spyFail = jasmine.createSpy('spy');
+    spySuccessfully = jasmine.createSpy('spy');
 
-      _$httpBackend_.flush();
+    credencials = {
+      email: 'email@valid.net.lcc',
+      password: 'passwordSample'
+    };
 
-      expect(result.user).toEqual(10);
+    authPromise = AuthenticateSrvc.login(credencials);
   }));
+
+  it('should login successfully with valid credencials', function() {
+    var response202 = {
+      token: 'the user authentication token',
+      user: 10,
+      createdAt: '09/04/2014 01:15:27 AM (-0300)',
+      updatedAt: '09/04/2014 01:15:27 AM (-0300)'
+    };
+
+    $httpBackend.whenPOST('http://localhost:1337/authenticate', credencials).respond(202, response202);
+    authPromise.then(spySuccessfully, spyFail);
+
+    $httpBackend.flush();
+
+    expect(spySuccessfully).toHaveBeenCalled();
+    expect(spyFail).not.toHaveBeenCalled();
+  });
+
+  it('should login fail with invalid credencials', function() {
+    $httpBackend.whenPOST('http://localhost:1337/authenticate', credencials).respond(401);
+    authPromise.then(spySuccessfully, spyFail);
+
+    $httpBackend.flush();
+
+    expect(spyFail).toHaveBeenCalled();
+    expect(spySuccessfully).not.toHaveBeenCalled();
+  });
 });
