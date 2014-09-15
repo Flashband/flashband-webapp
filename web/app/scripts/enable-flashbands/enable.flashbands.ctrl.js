09dd909d1d2c204a5f20b16f9c0f93feb669d2ec
Pablo Bender <pablo@bender.eti.br>
2014-09-15 12:16:10 -0300

    estrutura inicial da tela de cadastro de flashbands && configuração do ng-file-upload

--- web/app/scripts/enable-flashbands/enable.flashbands.ctrl.js
+++ web/app/scripts/enable-flashbands/enable.flashbands.ctrl.js
@@ -2,9 +2,10 @@
 
 angular.module('flashbandWebapp').controller('EnableFlashbandsCtrl', function ($scope, $upload) {
   $scope.onFileSelect = function($files) {
-    var onSuccess = function(data, status, headers, config) {
-      // file is uploaded successfully
-      console.log(data);
+    $scope.flashbandsEnabled = false;
+    var onSuccess = function(data) { //(data, status, headers, config)
+      $scope.flashbandsEnabled = true;
+      $scope.message = data.message;
     };
     var onProgress = function(evt) {
       console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
@@ -13,17 +14,15 @@ angular.module('flashbandWebapp').controller('EnableFlashbandsCtrl', function ($
     for (var i = 0; i < $files.length; i++) {
       var file = $files[i];
       $scope.upload = $upload.upload({
-        url: 'server/upload/url', //upload.php script, node.js route, or servlet url
-        //method: 'POST' or 'PUT',
+        method: 'POST',
+        url: 'http://localhost:1337/flashband/enable',
+        data: { name: $scope.name },
+        file: file, // or list of files ($files) for html5 only
+        fileFormDataName: 'flashbands' //or a list of names for multiple files (html5). Default is 'file', customize file formData name ('Content-Disposition'), server side file variable name. 
+        //fileName: 'flashbands-to-enable.csv', // or ['1.jpg', '2.jpg', ...] // to modify the name of the file(s)
         //headers: {'header-key': 'header-value'},
         //withCredentials: true,
-        data: {myObj: $scope.myModelObj},
-        file: file, // or list of files ($files) for html5 only
-        //fileName: 'doc.jpg' or ['1.jpg', '2.jpg', ...] // to modify the name of the file(s)
-        // customize file formData name ('Content-Disposition'), server side file variable name. 
-        //fileFormDataName: myFile, //or a list of names for multiple files (html5). Default is 'file' 
-        // customize how data is added to formData. See #40#issuecomment-28612000 for sample code
-        //formDataAppender: function(formData, key, val){}
+        //formDataAppender: function(formData, key, val){} //customize how data is added to formData. See #40#issuecomment-28612000 for sample code
       }).progress(onProgress).success(onSuccess);
       //.error(...)
       //.then(success, error, progress); 
--- web/test/unit/enable-flashbands/enable.flashbands.controller.spec.js
+++ web/test/unit/enable-flashbands/enable.flashbands.controller.spec.js
@@ -1,15 +1,52 @@
+/*globals describe, beforeEach, it, expect, inject*/
 'use strict';
 
 describe('Controller: EnableFlashbandsCtrl', function(){
-  var scope;
+  var scope, $httpBackend;
 
   beforeEach(module('flashbandWebapp'));
 
-  beforeEach(inject(function($rootScope, $controller) {
+  beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+    $httpBackend = _$httpBackend_;
+
     scope = $rootScope.$new();
 
     $controller('EnableFlashbandsCtrl', {
       $scope: scope
     });
   }));
+
+  describe('#onFileSelect', function() {
+    beforeEach(function() {
+      scope.onFileSelect([{}]);
+    });
+    it ('should set flashbandsEnabled on successfully upload', function() {
+      $httpBackend.whenPOST('http://localhost:1337/flashband/enable')
+        .respond(201, {message: 'Flashbands enabled successfully'});
+      expect(scope.flashbandsEnabled).toEqual(false);
+      $httpBackend.flush();
+      expect(scope.flashbandsEnabled).toEqual(true);
+    });
+    it ('should set message on successfully upload', function() {
+      $httpBackend.whenPOST('http://localhost:1337/flashband/enable')
+        .respond(201, {message: 'Flashbands enabled successfully'});
+      expect(scope.message).toBeFalsy();
+      $httpBackend.flush();
+      expect(scope.message).toEqual('Flashbands enabled successfully');
+    });
+  });
+
+  it('test with $httpBackend', function() {
+    $httpBackend.whenPOST('http://localhost:1337/flashband/enable').respond({message: 'Flashbands enabled successfully'});
+    expect(scope.onFileSelect).toBeDefined();
+    //$httpBackend.flush();
+    
+    //$scope.userPassword = 'wrong-password';
+    //$scope.signinClick();
+    //$httpBackend.flush();
+    //$timeout(function() {
+      //expect($scope.userId).toEqual('');
+      //expect($scope.userPassword).toEqual('');
+    //}, 2000);
+  });
 });
