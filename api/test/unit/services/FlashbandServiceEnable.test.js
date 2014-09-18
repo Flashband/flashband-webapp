779336a7449baf18b9b198c72c4539968de130a0
Pablo Bender <pablo@bender.eti.br>
2014-09-18 15:24:06 -0300

    creating test to ensure use of waterline association between FlashbandBatch -> Flashband using association methods .add(), .remove()

--- api/test/unit/services/FlashbandServiceEnable.test.js
+++ api/test/unit/services/FlashbandServiceEnable.test.js
@@ -17,6 +17,9 @@ describe('FlashbandService', function() {
           expect(saved).to.be.ok;
           expect(saved.flashbands).to.have.length(1, 'wrong number of associated flashbands');
           expect(saved.flashbands[0]).to.have.property('tag', '123456');
+          FlashbandBatch.findOne(flashbandBatch.id).then(function(flashbandBatch) {
+            expect(flashbandBatch).to.not.have.property('flashbands');
+          });
           done();
         }).fail(done);
       };
@@ -25,6 +28,15 @@ describe('FlashbandService', function() {
           FlashbandBatch.findOne({name:'lote 1'}).then(expectAssociation).fail(done);
         })
       .fail(done);
+    });
+    it('should ensure use waterline association FlashbandBatch -> Flashband', function(done) {
+      FlashbandService.enable([{ tag: '123456', serial: 1 }], 'lote 1', 'batch-file-content')
+        .then(function() {
+          FlashbandBatch.findOne({name:'lote 1'}).then(function(flashbandBatch) {
+            expect(flashbandBatch.flashbands).to.have.length(0);
+            done();
+          }).fail(done);
+        }).fail(done);
     });
     it('should save received batch file', function(done) {
       FlashbandService.enable([{ tag: '123456', serial: 1 }], 'lote 1', 'batch-file-content')
