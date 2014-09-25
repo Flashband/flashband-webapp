b78fb1c2b89cbf46632e9daa5511f5a56771aef9
Luiz Amorim <luiz.simples@gmail.com>
2014-09-25 02:08:47 -0300

    config tests E2e

--- web/test/protractor.conf.api.js
+++ web/test/protractor.conf.api.js
@@ -6,22 +6,27 @@ exports.config = {
 
   // Capabilities to be passed to the webdriver instance.
   capabilities: {
-    'browserName': 'PhantomJS'
+    'browserName': 'chrome',
+    'chromeOptions': {'args': ['--disable-extensions']}
   },
 
   baseUrl: 'http://localhost:3000',
-
+  rootElement: 'body',
   // Spec patterns are relative to the current working directly when
   // protractor is called.
   specs: ['test/e2e/**/*.js'],
 
   // Options to be passed to Jasmine-node.
   jasmineNodeOpts: {
+    // onComplete will be called just before the driver quits.
+    onComplete: null,
+    // If true, display spec names.
+    isVerbose: true,
+    // If true, print colors to the terminal.
     showColors: true,
-    defaultTimeoutInterval: 30000
-  },
-
-  onPrepare: function () {
-    browser.mock = function () {}
+    // If true, include stack traces in failures.
+    includeStackTrace: true,
+    // Default time to wait in ms before a test fails.
+    defaultTimeoutInterval: 10000
   }
 };
