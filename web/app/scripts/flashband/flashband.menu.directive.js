ffc4bf9f7f7dfb25ca3539222ba0fda93c36ee99
Luiz Amorim <luiz.simples@gmail.com>
2014-10-02 23:34:48 -0300

    item menu falsy

--- web/app/scripts/flashband/flashband.menu.directive.js
+++ web/app/scripts/flashband/flashband.menu.directive.js
@@ -1,20 +1,20 @@
 'use strict';
 
-angular.module('flashbandWebapp').directive('flashbandMenu', function flashbandMenu () {
+angular.module('flashbandWebapp').directive('flashbandMenu', function flashbandMenu() {
   return {
     restrict: 'AE',
     templateUrl: 'partials/menu.html',
-    link: function(scope) {
-      scope.menus = [{
-        sref: 'enable-flashbands',
-        label: 'FLASHBAND.MENU.FLASHBANDS'
-      }, {
-        sref: 'showgoer',
-        label: 'FLASHBAND.MENU.SHOWGOER'
-      }, {
-        sref: 'dashboard',
-        label: 'outro item do menu'
-      }];
+    link: function (scope) {
+      scope.menus = [
+        {
+          sref: 'enable-flashbands',
+          label: 'FLASHBAND.MENU.FLASHBANDS'
+        },
+        {
+          sref: 'showgoer',
+          label: 'FLASHBAND.MENU.SHOWGOER'
+        }
+      ];
 
       scope.menus.push({
         sref: 'logout',
