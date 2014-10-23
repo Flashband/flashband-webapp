01463fefcd16c1774987f93d46749ed370d7c439
Luiz Amorim <luiz.simples@gmail.com>
2014-10-23 19:30:06 -0200

    should list all showgoers

--- web/app/scripts/status/status.controller.js
+++ web/app/scripts/status/status.controller.js
@@ -2,6 +2,11 @@
 
 angular.module('flashbandWebapp').controller('StatusCtrl', function ($scope, FlashbandRestFact) {
   $scope.totFlashbands = 0;
+  $scope.listShowgoers = [];
+
+  FlashbandRestFact.getConnection().all('showgoer').getList().then(function(list) {
+    $scope.listShowgoers = list;
+  });
 
   FlashbandRestFact.getConnection().one('flashband').one('summary').get().then(function(res) {
     $scope.totFlashbands = res.total;
