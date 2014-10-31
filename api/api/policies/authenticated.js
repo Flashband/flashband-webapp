c552164860befe231a869279de05ede4188969a6
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2014-10-31 20:22:27 -0200

    Accept socket conection requests as authenticated ones.

--- api/api/policies/authenticated.js
+++ api/api/policies/authenticated.js
@@ -1,6 +1,13 @@
 'use strict';
 
 module.exports = function (req, res, next) {
+
+  // @todo: handle authentication over sockets. I suggest modifing the whole
+  // authentication service and system to match Kasper Isager's generator for
+  // authentication using Passport, which handles sockets pretty well.
+  // Generator: https://github.com/kasperisager/sails-generate-auth
+  if (req.isSocket) return next();
+
   var spliter = req.headers.authorization.split('Token token=');
   if (!req.headers.authorization) { return res.unauthorized(); }
 
